731ca316eaa96a564d2a13ba06b87137f0497cc9
Andrea Casagranda <ybblkblt@yahoo.com>
2022-09-04 15:17:06 -0400

    fix title

--- TrailRecommend.py
+++ TrailRecommend.py
@@ -212,7 +212,7 @@ def print_list(interest_list):
 all_finished = False
 while not all_finished:
     print('\n\n/\  _FIND A LOCAL TRAIL_  /\______ ')
-    print('  \/                      \/\n\n')
+    print('  \/                    \/\n\n')
     print("We'll list all local trails that fit your requirements.\n")
     finished = False
     length_choice = False
